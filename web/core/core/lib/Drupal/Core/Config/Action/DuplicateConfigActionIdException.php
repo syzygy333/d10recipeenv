1c689b96eb56781c3a9bd466c90f0f68b8aafbf4
Brian Hogue <bhogue@oomphinc.com>
2023-05-25 17:14:10 -0400

    Remove ignored items

--- web/core/core/lib/Drupal/Core/Config/Action/DuplicateConfigActionIdException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\Core\Config\Action;
-
-/**
- * Exception thrown if there are conflicting shorthand action IDs.
- */
-class DuplicateConfigActionIdException extends \RuntimeException {
-}
